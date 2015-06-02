a5f1f2001beb36f864a901335c4d94f9fa9b746b
Benjamin Schollnick <Benjamin@schollnick.net>
2015-06-02 12:54:54 -0400

    Updating to remove dependencies from GITHub
    * Working on updating setup.py
    * Removing dependencies from GITHUB

---
+++ tools/uninstall_dependencies.sh
@@ -0,0 +1,13 @@
+pip uninstall  directory_caching
+pip uninstall  jinja
+pip uninstall  markdown
+pip uninstall  natsort
+pip uninstall  passlib
+pip uninstall  Pillow
+pip uninstall  pybonjour
+pip uninstall  rarfile
+pip uninstall  rarfile
+pip uninstall  scandir
+pip uninstall  twisted
+pip uninstall  txbonjour
+pip uninstall  unidecode
