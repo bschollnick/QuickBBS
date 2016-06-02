0313856012243f5b14a707ce8c3211be16bd8286
Benjamin Schollnick <Benjamin@schollnick.net>
2016-06-02 06:05:02 -0400

    First checkin of Django Code.

---
+++ django/quickbbs/quickbbs/urls.py
@@ -0,0 +1,35 @@
+"""quickbbs URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.views.generic import RedirectView
+#import django.contrib.auth.views
+import frontend, frontend.views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^(?i)albums/', frontend.views.viewgallery),
+    url(r'^thumbnails/', frontend.views.thumbnails),
+    url(r'^resources/', frontend.views.resources),
+    url(r'^accounts/', include('allauth.urls')),
+    url(r'^$', RedirectView.as_view(url="/albums")),
+#    url(r'^login/$', django.contrib.auth.views.login),
+#    url(r'^logout/$', django.contrib.auth.views.logout),
+    #    url(r'^albums/', include('frontend.urls'))
+#    url(r'^thumbnails/', include('frontend.urls')),
+]
+
+REGISTRATION_OPEN = True
