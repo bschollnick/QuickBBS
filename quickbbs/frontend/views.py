17ffea18d54ac533870ab814da866edca86bbdaa
Benjamin Schollnick <Benjamin@schollnick.net>
2019-02-04 06:04:03 -0500

    change to support django-icons

--- quickbbs/frontend/views.py
+++ quickbbs/frontend/views.py
@@ -3,11 +3,6 @@
 Django views for QuickBBS Gallery
 """
 from __future__ import absolute_import, print_function, unicode_literals
-import quickbbs.settings
-if quickbbs.settings.SILK:
-    from silk.profiling.profiler import silk_profile
-#else:
-#    from frontend.utilities import silk_profile
 
 import datetime
 from itertools import chain
@@ -47,7 +42,6 @@ from frontend.web import (verify_login_status, detect_mobile,
 from quickbbs.models import index_data#, Thumbnails_Archives
 
 from urllib.parse import unquote
-import django_icons.templatetags.icons
 
 import logging
 log = logging.getLogger(__name__)
@@ -70,6 +64,7 @@ def return_prev_next(fqpn, currentpath, sorder):
     Replace the old system, with Django pagination.
     """
     Parent_path = Path(fqpn).parent
+    #dirlist = [ item.lower() for item in os.listdir(Parent_path) if os.path.isdir(os.path.join(Parent_path, item)) ]
     current_folder_name = os.path.basename(Path(fqpn)).lower()
     prevdir = ""
     nextdir = ""
@@ -103,7 +98,6 @@ def return_prev_next(fqpn, currentpath, sorder):
     return (prevdir, nextdir)
 
 @vary_on_headers('User-Agent', 'Cookie', 'Request')
-#@silk_profile(name='views.thumbnails')
 def thumbnails(request, t_url_name=None):
     """
     Serve the thumbnail resources
@@ -111,35 +105,40 @@ def thumbnails(request, t_url_name=None):
     URL -> thumbnails/(?P<t_url_name>.*)
     """
 #
+#    print ("Entering Thumbnails")
     t_url_name = str(t_url_name).strip().replace("/", "")
-    if is_valid_uuid(t_url_name):
-        index_qs = index_data.objects.filter(uuid=t_url_name,
-                                             ignore=False, delete_pending=False)
-        if index_qs.count() > 1:
-            check_dup_thumbs(t_url_name)
-            index_qs = index_data.objects.filter(uuid=t_url_name)
-        entry = index_qs[0]
-        fs_item = os.path.join(configdata["locations"]["albums_path"],
-                               entry.fqpndirectory[1:].lower(),
-                               entry.name)
-#        fs_item = fs_item.replace("//", "/")
-    #    check_dup_thumbs(e_uuid)
-        # entry.fqpndirectory[1:] since a / in the root cancels the configdata
-        # albums path
+    if not is_valid_uuid(t_url_name):
+        return HttpResponseBadRequest(content="Non-UUID thumbnail request.")
+    #e_uuid = t_url_name
+    index_qs = index_data.objects.filter(uuid=t_url_name)
+    if index_qs.count() > 1:
+        check_dup_thumbs(t_url_name)
+        index_qs = index_data.objects.filter(uuid=t_url_name)
+    entry = index_qs[0]
+    fs_item = os.path.join(configdata["locations"]["albums_path"],
+                           entry.fqpndirectory[1:].lower(),
+                           entry.name)
+    fs_item = fs_item.replace("//", "/")
+#    check_dup_thumbs(e_uuid)
+    page = int(g_option(request, "page", 0))
+    if page == "":
+        page = 0
+    # entry.fqpndirectory[1:] since a / in the root cancels the configdata
+    # albums path
 
-        if entry.directory:
-            #print ("Directory", entry.name)
-            return new_process_dir(entry)
-        elif entry.file_tnail:
-            return new_process_img(entry, request)
-        elif entry.archives:
-            page = int(g_option(request, "page", 0))
-            return new_process_archive(entry, request, page)
+    if entry.directory:
+        #print ("Directory", entry.name)
+        return new_process_dir(entry)
+    elif entry.archives:
+        #print("is archive")
+        return new_process_archive(entry, request, page)
+    elif entry.file_tnail:
+        return new_process_img(entry, request)
 
-    return HttpResponseBadRequest(content="Bad UUID or %s Unidentifable file." % fs_item)
+    return HttpResponseBadRequest(content="Unidentifable file.")
 
+#@vary_on_headers('User-Agent', 'Cookie')
 @vary_on_headers('User-Agent', 'Cookie', 'Request')
-#@silk_profile(name='View new_Viewgallery')
 def new_viewgallery(request):
     """
     View the requested Gallery page
@@ -147,6 +146,8 @@ def new_viewgallery(request):
    # start_time = time.time()
     context = {}
     paths = {}
+#    context["filetypes"] = configdata["filetypes"]
+#    context["ftypemap"] = ftypes.ftypes
     context["small"] = g_option(request,
                                 "size",
                                 configdata["configuration"]["small"])
@@ -159,10 +160,13 @@ def new_viewgallery(request):
     context["user"] = request.user
     context["mobile"] = detect_mobile(request)
     request.path = request.path.lower().replace(os.sep, r"/")
+#    paths["webpath"] = request.path.lower().replace(os.sep, r"/")
     paths["webpath"] = ensures_endswith(request.path, "/")
 
     request, context = sort_order(request, context)
     context["webpath"] = paths["webpath"]
+#    if not context["webpath"].endswith("/"):
+#        context["webpath"] += "/"
     context["fromtimestamp"] = datetime.datetime.fromtimestamp
     paths["album_viewing"] = configdata["locations"]["albums_path"] + paths["webpath"]
 
@@ -181,12 +185,10 @@ def new_viewgallery(request):
     #elif isdir(paths["album_viewing"]):
     #
     # The only thing left is a directory.
+    log.info("Reading from Disk")
     read_from_disk(paths["webpath"], skippable=True) # new_viewgallery
     index = get_db_files(context["sort_order"], paths["webpath"])
-
-#    index = list(index.order_by(*SORT_MATRIX[context["sort_order"]]))
-#   already sorted by get_db_files call.
-
+    index = list(index.order_by(*SORT_MATRIX[context["sort_order"]]))
     context["current_page"] = request.GET.get("page", 1)
     chk_list = Paginator(index, 30)
     context["page_cnt"] = list(range(1, chk_list.num_pages + 1))
@@ -222,7 +224,6 @@ def new_viewgallery(request):
 
 #@vary_on_headers('User-Agent', 'Cookie')
 @vary_on_headers('User-Agent', 'Cookie', 'Request')
-#@silk_profile(name='View new_Viewitem')
 def new_viewitem(request, i_uuid):
     i_uuid = str(i_uuid).strip().replace("/", "")
     context = {}
@@ -273,7 +274,6 @@ def new_viewitem(request, i_uuid):
 
 
 @vary_on_headers('User-Agent', 'Cookie', 'Request')
-#@silk_profile(name='View new_download')
 def new_download(request, d_uuid=None):
     page = request.GET.get('page', None)
     if page is None:
@@ -292,7 +292,6 @@ def new_download(request, d_uuid=None):
                                  download.name)
 
 @vary_on_headers('User-Agent', 'Cookie', 'Request')
-#@silk_profile(name='View new_view_archive')
 def new_view_archive(request, i_uuid):
     context = {}
     i_uuid = str(i_uuid).strip().replace("/", "")
@@ -309,7 +308,6 @@ def new_view_archive(request, i_uuid):
     context["webpath"] = entry.fqpndirectory.lower().replace("//", "/")
     context["webpath"] = ensures_endswith(context["webpath"], "/")
     context["fromtimestamp"] = datetime.datetime.fromtimestamp
-    context["djicons"] = django-icons.templatetags.icons.icon
     arc_filename = configdata["locations"]["albums_path"] +  \
         context["webpath"].replace("/", os.sep).replace("//", "/") + entry.name
     archive_file = archives.id_cfile_by_sig(arc_filename)
@@ -363,8 +361,7 @@ def new_archive_item(request, i_uuid):
 #    print (context["current_page"])
     archive_file = archives.id_cfile_by_sig(item_fs)
     archive_file.get_listings()
-#    context["pagecount"] = len(archive_file.listings)-1
-    context["pagecount"] = archive_file.listings.count()-1
+    context["pagecount"] = len(archive_file.listings)-1
     context["item"] = entry
     item_list = Paginator(archive_file.listings, 1)
     context["page_contents"] = item_list.page(context["current_page"]+1)
@@ -390,6 +387,202 @@ def new_archive_item(request, i_uuid):
                       #using="Jinja2")
     patch_vary_headers(response, ["sort-%s" % context["sort_order"]])
     return response
+
+@vary_on_headers('User-Agent', 'Cookie')
+def viewarchive(request):
+    """
+    Serve archive files
+    """
+    context = {}
+    paths = {}
+    request, context = sort_order(request, context)
+    paths["item_fs"] = configdata["locations"]["albums_path"]\
+        + unquote(request.path.replace("/", os.sep))
+    paths["item_path"], paths["item_name"] = os.path.split(
+        paths["item_fs"].lower())   # path to archive
+    paths["thumb_path"] = paths["item_path"].replace("%salbums" % os.sep,
+                                                     "%sthumbnails" % os.sep)
+    paths["web_path"] = paths["item_path"].replace(
+        configdata["locations"]["albums_path"].lower(), "")
+    paths["web_thumbpath"] = paths["web_path"].replace("/albums",
+                                                       "/thumbnails") + r"/"
+    context["current_page"] = request.GET.get("page", 1)
+
+    if "a_item" in request.GET:
+        return archive_item(request)
+
+    archive_file = archives.id_cfile_by_sig(paths["item_fs"])
+    archive_file.get_listings()
+    archive_file.listings.sort()
+    global_listings = archive_file.listings
+
+    listings = []
+    for count, filename in enumerate(global_listings):
+        #               0,          1,          ,2
+        #   Listings = filename, zip fqfn, web thumbnail path (Med & Large),
+
+        #       3,                              4
+        #   thumbnail fs path (med & large), background color
+
+        fext = os.path.splitext(filename)[1][1:]
+        if fext in configdata["filetypes"]:
+            if configdata["filetypes"][fext][1].strip() != "None":
+                bgcolor = configdata["filetypes"][fext][0]
+            else:
+                bgcolor = configdata["filetypes"]["none"][0]
+
+        listings.append((filename,
+                         paths["item_fs"],
+                         paths["web_thumbpath"] + paths["item_name"],
+                         paths["web_thumbpath"] + paths["item_name"],
+                         bgcolor,
+                         count + 1))
+
+    context["current_page"] = request.GET.get("page", 1)
+    chk_list = Paginator(listings, 30)
+    context["page_cnt"] = list(range(1, chk_list.num_pages + 1))
+    context["up_uri"] = "/".join(request.get_raw_uri().split("/")[0:-1])
+    context["gallery_name"] = os.path.split(request.path_info)[-1]
+    try:
+        context["pagelist"] = chk_list.page(context["current_page"])
+    except PageNotAnInteger:
+        context["pagelist"] = chk_list.page(1)
+    except EmptyPage:
+        context["pagelist"] = chk_list.page(chk_list.num_pages)
+    context["all_listings"] = global_listings
+
+    context["prev_uri"], context["next_uri"] = return_prev_next(
+        paths["item_path"], paths["web_path"], context["sort_order"])
+    context["webpath"] = paths["web_path"] + "/%s" % paths["item_name"]
+    template = loader.get_template('frontend/archive_gallery.html')
+    return HttpResponse(template.render(context, request))
+
+def new_view_archive_item(request, i_uuid):
+    context = {}
+    if not is_valid_uuid(i_uuid.strip().replace("/", "")):
+        return HttpResponseBadRequest(content="Non-UUID thumbnail request.")
+
+    request, context = sort_order(request, context)
+    e_uuid = i_uuid.strip().replace("/", "")
+    index_qs = index_data.objects.filter(uuid=e_uuid)
+    entry = index_qs[0]
+
+    context["webpath"] = entry.fqpndirectory.lower().replace("//", "/")
+#    context["up_uri"] = "/".join(request.get_raw_uri().split("/")[0:-1])
+    context["up_uri"] = entry.fqpndirectory.lower()
+    read_from_disk(context["webpath"].strip(), skippable=True)
+
+    index_qs = get_db_files(context["sort_order"], context["webpath"]).filter(uuid=e_uuid)
+#    arc_qs = Thumbnails_Archives.objects.filter()
+    context["page"] = 1
+    for counter, data in enumerate(index_qs, start=1):
+        if str(data.uuid) == e_uuid:
+            context["page"] = counter
+            break
+
+    item_list = Paginator(index_qs, 1)
+    context["pagecount"] = item_list.count
+    context["page_contents"] = item_list.page(context["page"])
+    context["item"] = entry
+
+    if context["page_contents"].has_next():
+        context["next"] = index_qs[context["page_contents"].next_page_number()-1].uuid
+    else:
+        context["next"] = ""
+
+    if context["page_contents"].has_previous():
+        context["previous"] = index_qs[context["page_contents"].previous_page_number()-1].uuid
+    else:
+        context["previous"] = ""
+#
+    context["first"] = index_qs[0].uuid
+    context["last"] = index_qs[index_qs.count()-1].uuid
+#        context["last"] = catalog_qs[context["page_contents"].page_range[-1]].uuid
+    response = render(request,
+                      "frontend/archive_item.html",
+                      context)#,
+                      #using="Jinja2")
+    patch_vary_headers(response, ["sort-%s" % context["sort_order"]])
+    return response
+
+@vary_on_headers('User-Agent', 'Cookie')
+def archive_item(request):
+    """
+    Serve the gallery items
+    """
+    context = {}
+    paths = {}
+    context["mobile"] = detect_mobile(request)
+    request, context = sort_order(request, context)
+    context["small"] = g_option(request,
+                                "size",
+                                configdata["configuration"]["small"])
+    context["medium"] = g_option(request,
+                                 "size",
+                                 configdata["configuration"]["medium"])
+    context["large"] = g_option(request,
+                                "size",
+                                configdata["configuration"]["large"])
+    paths["archive_item"] = int(g_option(request, "a_item", 1)) - 1
+    paths["item_fs"] = configdata["locations"]["albums_path"]\
+        + unquote(request.path.replace("/",
+                                       os.sep))
+    paths["item_path"], paths["item_name"] = os.path.split(
+        paths["item_fs"].lower())
+    paths["thumb_path"] = paths["item_path"].replace("%salbums" % os.sep,
+                                                     "%sthumbnails" % os.sep)
+    paths["web_path"] = paths["item_path"].replace(
+        configdata["locations"]["albums_path"].lower(), "")
+    paths["web_thumbpath"] = paths["web_path"].replace("/albums",
+                                                       "/thumbnails") + r"/"
+
+
+    archive_file = archives.id_cfile_by_sig(paths["item_fs"])
+    archive_file.get_listings()
+    archive_file.listings.sort()
+    global_listings = archive_file.listings
+
+    listings = []
+    for count, filename in enumerate(global_listings):
+        #               0,          1,          ,2
+        #   Listings = filename, zip fqfn, web thumbnail path (Med & Large),
+
+        #       3,                              4
+        #   thumbnail fs path (med & large), background color
+        fext = os.path.splitext(filename)[1][1:]
+        if fext in configdata["filetypes"]:
+            if configdata["filetypes"][fext][1] != "None":
+                bgcolor = configdata["filetypes"][fext][0]
+            else:
+                bgcolor = configdata["filetypes"]["none"][0]
+
+        listings.append((filename,
+                         paths["item_fs"],
+                         paths["web_thumbpath"] + paths["item_name"],
+                         paths["web_thumbpath"] + paths["item_name"],
+                         bgcolor,
+                         count + 1))
+
+    context["current_page"] = request.GET.get("page", 1)
+
+    context["current_page"] = request.GET.get("a_item")
+    chk_list = Paginator(listings, 1)
+    context["page_cnt"] = list(range(1, chk_list.num_pages + 1))
+    context["up_uri"] = "/".join(request.get_raw_uri().split("/")[0:-1])
+    context["gallery_name"] = os.path.split(request.path_info)[-1]
+    try:
+        context["pagelist"] = chk_list.page(context["current_page"])
+    except PageNotAnInteger:
+        context["pagelist"] = chk_list.page(1)
+    except EmptyPage:
+        context["pagelist"] = chk_list.page(chk_list.num_pages)
+    context["all_listings"] = global_listings
+
+    context["prev_uri"], context["next_uri"] = return_prev_next(
+        paths["item_path"], paths["web_path"], context["sort_order"])
+    context["webpath"] = paths["web_path"] + "/%s" % paths["item_name"]
+    template = loader.get_template('frontend/archive_item.html')
+    return HttpResponse(template.render(context, request))
 
 
 if 'runserver' in sys.argv:
