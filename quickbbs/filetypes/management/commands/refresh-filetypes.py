aa52ec32d55f7d28e174112964907cfe8caf7c32
Benjamin Schollnick <benjamin@schollnick.net>
2022-11-22 19:51:18 -0500

    Continuing with requirements updates, and more work on filetypes
    * Resolved the issues with Django-icon
    * Identified the issue with django-jinja, which the maintainer states was been fixed.  Trying to clarify, since it still doesn't work with Jinja3.1x.x
    * moved the admin refresh-filetypes to populate the new table, need to continue to port over the non-admin code to use new table.

---
+++ quickbbs/filetypes/management/commands/refresh-filetypes.py
@@ -0,0 +1,83 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from filetypes.models import filetypes as new_filetypes
+from filetypes.constants import ftypes
+import filetypes.constants as constants
+from filetypes.ftypes import *
+
+class Command(BaseCommand):
+    def refresh_filetypes(self):
+        for ext in constants._movie:
+            new_filetypes.objects.update_or_create(fileext=ext,
+                                               defaults={"generic":True,
+                                                         "icon_filename":"MovieIcon100.jpg",
+                                                         "color":"CCCCCC",
+                                                         "filetype":ftypes['movie'],
+                                                         "is_movie":True}
+                                                         )
+
+        for ext in constants._archives:
+            new_filetypes.objects.update_or_create(fileext=ext,
+                                               defaults={"generic":True,
+                                                         "icon_filename":"1431973824_compressed.png",
+                                                         "color":"b2dece",
+                                                         "filetype":ftypes['archive'],
+                                                         "is_archive":True})
+
+        for ext in constants._html:
+            new_filetypes.objects.update_or_create(fileext=ext,
+                                               defaults={"generic":True,
+                                               "icon_filename":"1431973779_html.png",
+                                               "color":"fef7df", "filetype":ftypes['html']})
+
+        for ext in constants._graphics:
+            new_filetypes.objects.update_or_create(fileext=ext,
+                                               defaults={"generic":False,
+                                               "color":"FAEBF4", "filetype":ftypes['image'],
+                                               "is_image":True})
+
+        for ext in constants._text:
+            new_filetypes.objects.update_or_create(fileext=ext,
+                                               defaults={"generic":True,
+                                               "icon_filename":"1431973815_text.PNG",
+                                               "color":"FAEBF4", "filetype":ftypes['image']})
+
+        new_filetypes.objects.update_or_create(fileext=".pdf",
+                                           defaults={"generic":False,
+                                           "color":"FDEDB1", "filetype":ftypes['image'],
+                                           "is_pdf":True})
+
+        new_filetypes.objects.update_or_create(fileext=".epub",
+                                           defaults={"generic":True,
+                                           "icon_filename":"epub-logo.gif",
+                                           "color":"FDEDB1", "filetype":ftypes['epub']})
+
+        new_filetypes.objects.update_or_create(fileext=".dir",
+                                            defaults={"generic":False,
+                                           "color":"DAEFF5", "filetype":ftypes['dir'],
+                                           "is_dir":True})
+
+        new_filetypes.objects.update_or_create(fileext=".none", defaults={"generic":True,
+                                           "icon_filename":"1431973807_fileicon_bg.png",
+                                           "color":"FFFFFF", "filetype":ftypes['unknown']})
+
+
+    def add_arguments(self, parser):
+        # Positional arguments
+#        parser.add_argument('poll_ids', nargs='+', type=int)
+
+        # Named (optional) arguments
+        parser.add_argument(
+            '--refresh-filetypes',
+            action='store_true',
+            help='Add, Refresh and revise the FileType table',
+        )
+
+    def handle(self, *args, **options):
+        # ...
+        try:
+             self.refresh_filetypes()
+        except:
+             print("Unable to validate or create FileType database table.")
+        #     pass
+
--- quickbbs/quickbbs/jinjaenv.py
+++ quickbbs/quickbbs/jinjaenv.py
@@ -1,14 +1,15 @@
 #from __future__ import absolute_import  # Python 2 only
 from jinja2 import Environment
-from django.contrib.staticfiles.storage import staticfiles_storage
+#from django.contrib.staticfiles.storage import staticfiles_storage
+from django_icons.templatetags.icons import icon_tag
+from django.templatetags.static import static
 from django.urls import reverse
-from django_icons.templatetags.icons import icon
 
 def environment(**options):
     env = Environment(**options)
     env.globals.update({
-       'static': staticfiles_storage.url,
+       'static': static,
        'url': reverse,
-       'icon':icon,
+       'icon':icon_tag,
     })
     return env
