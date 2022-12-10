e9ec496af8be8dda72d957295bb54912aad7724f
Benjamin Schollnick <benjamin@schollnick.net>
2022-12-10 08:17:28 -0500

    Removed jquery lazy image loader javascript since what we've been using is now part of img (img loading='lazy').
    Experimenting with htmx, unicorn, and other ajax solutions to speed things up.

--- quickbbs/quickbbs/settings.py
+++ quickbbs/quickbbs/settings.py
@@ -23,7 +23,7 @@ machine_name = socket.gethostname().lower()
 print("Running on %s" % machine_name)
 
 DEBUG = True
-DEBUG = not DEBUG
+# DEBUG = not DEBUG
 print("Debug is ", DEBUG)
 
 # DEBUG_TOOLBAR = True
@@ -105,6 +105,7 @@ INSTALLED_APPS += [
     'filetypes',
     #    'fontawesome-free',
     'django_icons',
+    'django_unicorn',
     'django_jinja.contrib._humanize'
 ]
 
