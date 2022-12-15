17c1d1df2aa8ee0d254b6287ecad9f6ca887cfe0
Benjamin Schollnick <benjamin@schollnick.net>
2022-12-14 22:42:24 -0500

    restored django-toolbar-debug functionality when debug mode is enabled.

--- quickbbs/quickbbs/settings.py
+++ quickbbs/quickbbs/settings.py
@@ -30,8 +30,9 @@ DEBUG = False
 #DEBUG = not DEBUG
 print("Debug is ", DEBUG)
 
+
 # DEBUG_TOOLBAR = True
-DEBUG_TOOLBAR = False
+DEBUG_TOOLBAR = DEBUG
 
 JQUERY_VERSION = '3.6.1'
 JQUERY_URI = f'https://cdnjs.cloudflare.com/ajax/libs/jquery/{JQUERY_VERSION}/jquery.slim.min.js'
@@ -298,3 +299,21 @@ DJANGO_ICONS = {
     },
 
 }
+
+# if DEBUG:
+#     DEBUG_TOOLBAR_PANELS = [
+#         'debug_toolbar.panels.history.HistoryPanel',
+#         'debug_toolbar.panels.versions.VersionsPanel',
+#         'debug_toolbar.panels.timer.TimerPanel',
+#         'debug_toolbar.panels.settings.SettingsPanel',
+#         'debug_toolbar.panels.headers.HeadersPanel',
+#         'debug_toolbar.panels.request.RequestPanel',
+#         'debug_toolbar.panels.sql.SQLPanel',
+#         'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+#         'debug_toolbar.panels.templates.TemplatesPanel',
+#         'debug_toolbar.panels.cache.CachePanel',
+#         'debug_toolbar.panels.signals.SignalsPanel',
+#         'debug_toolbar.panels.logging.LoggingPanel',
+#         'debug_toolbar.panels.redirects.RedirectsPanel',
+#         'debug_toolbar.panels.profiling.ProfilingPanel',
+#     ]
