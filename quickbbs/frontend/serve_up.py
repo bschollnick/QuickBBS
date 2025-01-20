350bfbcaca10e6d5d0efb04110e194d65d5a5532
Benjamin Schollnick <benjamin@schollnick.net>
2025-01-20 01:47:28 -0500

    fix regression that eliminated static portion

--- quickbbs/frontend/serve_up.py
+++ quickbbs/frontend/serve_up.py
@@ -14,11 +14,14 @@ async def static_or_resources(request, pathstr=None) -> object:
     Serve the resources or static file
     """
     if pathstr is not None:
-        album_viewing = os.path.join(settings.RESOURCES_PATH, pathstr)
-        try:
+        resource_file = os.path.join(settings.RESOURCES_PATH, pathstr)
+        static_file = os.path.join(settings.STATIC_ROOT, pathstr)
+        if os.path.exists(resource_file):
             return serve(
-                request, os.path.basename(album_viewing), os.path.dirname(album_viewing)
-            )
-        except IOError:
-            print(f"File Not Found - {album_viewing}")
+                request, os.path.basename(resource_file), os.path.dirname(resource_file)
+                )
+        else:
+            return serve(
+                    request, os.path.basename(static_file), os.path.dirname(static_file)
+                )
     return Http404
