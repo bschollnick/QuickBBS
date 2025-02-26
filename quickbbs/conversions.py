7602960356bae308818b1657ba05fd1e11e204c3
Benjamin Schollnick <benjamin@schollnick.net>
2025-02-26 14:23:41 -0500

    up to latest

--- quickbbs/conversions.py
+++ quickbbs/conversions.py
@@ -7,11 +7,75 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quickbbs.settings")
 django.setup()
 
-from quickbbs.models import (
-    IndexDirs,
-    convert_text_to_md5_hdigest,
-    IndexData,
-)
+from quickbbs.models import IndexData, IndexDirs, convert_text_to_md5_hdigest
+from thumbnails.models import ThumbnailFiles
+import pprint
+import os
+# for entry in IndexData.objects.all():
+#     # pprint.pprint(entry.__dict__)
+#     fs_item = os.path.join(entry.fqpndirectory, entry.name).title().strip()
+#     if not os.path.exists(fs_item):
+#         print("deleting")
+#         entry.delete()
+#         continue
+
+for entry in IndexDirs.objects.all():
+#     # pprint.pprint(entry.__dict__)
+     fs_item = os.path.join(entry.fqpndirectory).title().strip()
+     if not os.path.exists(fs_item):
+        print("deleting")
+        entry.delete()
+        continue
+
+    #entry.file_sha256 = entry.get_file_sha(fqfn=fs_item)
+    #print("updating")
+    #entry.save(update_fields=["file_sha256"])
+#  index_qs = (
+#         IndexData.objects.prefetch_related("new_ftnail")
+#         .prefetch_related("filetype")
+#         .filter(uuid=tnail_id)
+#     )
+#     if not index_qs.exists():
+#         # does not exist
+#         print(tnail_id, "File not found - No records returned.")
+#         return Http404
+
+#     thumbsize = request.GET.get("size", "small").lower()
+#     entry = index_qs[0]
+#     fs_item = os.path.join(entry.fqpndirectory, entry.name).title().strip()
+#     fs_item_hash = ThumbnailFiles.convert_text_to_md5_hdigest(fs_item)
+#     # fname = os.path.basename(entry.name).title().strip()
+#     if entry.new_ftnail:
+#         if entry.new_ftnail.thumbnail_exists(size=thumbsize):
+#             return entry.new_ftnail.send_thumbnail(
+#                 filename_override=None, fext_override=".jpg", size=thumbsize
+#             )
+
+# for uuid in IndexData.objects.filter(new_ftnail__sha256_hash=None).values_list("uuid", flat=True):
+#     entry = (
+#         IndexData.objects.prefetch_related("new_ftnail")
+#         .prefetch_related("filetype")
+#         .filter(uuid=uuid)
+#     )[0]
+#     fs_item = os.path.join(entry.fqpndirectory, entry.name).title().strip()
+#     if not entry.new_ftnail:
+#         continue # no thumbnail has been created
+# #    print(fs_item)
+#     if not entry.new_ftnail.thumbnail_exists(size="small"):
+#         print("Skipping")
+#         continue
+#     fs_item_hash = ThumbnailFiles.convert_text_to_md5_hdigest(fs_item)
+#     #print(fs_item_hash)
+#     Thumbnail = ThumbnailFiles.objects.filter(fqpn_hash=fs_item_hash)[0]
+#     #Thumbnail = ThumbnailFiles.objects.filter(fqpn_filename=fs_item)[0]
+#     Thumbnail.sha256_hash = entry.file_sha256
+#     Thumbnail.save(update_fields=["sha256_hash"])
+# Thumbnail.save(update_fields=["sha256_hash"])
+# tnail = ThumbnailFiles.objects.filter(pk=IndexFile.new_ftnail)
+# IndexFile.new_ftnail.save(update_fields=["sha256_hash"])
+
+# for thumbnail in ThumbnailFiles.objects.filter(sha256_hash=None):
+#    parent_file = IndexData.objects.filter(fqpnfile=thumbnail.fqpn_filename)
 
 # new_dir_index = Index_Dirs
 
