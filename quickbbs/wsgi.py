0613ca81d1b6a31bc0a7283593da44665086a2c7
Benjamin Schollnick <benjamin@schollnick.net>
2025-10-01 08:06:00 -0400

    moving to an asgi  ready design

--- quickbbs/wsgi.py
+++ quickbbs/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 import sys
 
-quickbbs_path = r"/Volumes/4TB_Drive/gallery/quickbbs"
+quickbbs_path = r"/Volumes/C-8TB/gallery/quickbbs"
 sys.path.append(quickbbs_path)
 from django.core.wsgi import get_wsgi_application
 
@@ -18,6 +18,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quickbbs.settings")
 
 application = get_wsgi_application()
 
-# gunicorn -b 0.0.0.0:8888 --reload --pythonpath /Volumes/4TB_Drive/gallery/quickbbs,. quickbbs.wsgi
-# gunicorn --worker-class eventlet -b 0.0.0.0:8888 --workers 5 --threads 5 --graceful-timeout 45 --reload --pythonpath /Volumes/4TB_Drive/gallery/quickbbs,. quickbbs.wsgi
+# gunicorn -b 0.0.0.0:8888 --reload --pythonpath /Volumes/C-8TB/gallery/quickbbs,. quickbbs.wsgi
+# gunicorn --worker-class eventlet -b 0.0.0.0:8888 --workers 5 --threads 5 --graceful-timeout 45 --reload --pythonpath /Volumes/C-8TB/gallery/quickbbs,. quickbbs.wsgi
 # uwsgi --chdir=/Volumes/C-8TB/Gallery/quickbbs/quickbbs --module=quickbbs.wsgi --env DJANGO_SETTINGS_MODULE=quickbbs.settings --master --pidfile=./uswgi.pid --socket=0.0.0.0:8888 --protocol=http -b 65535 --processes=5
